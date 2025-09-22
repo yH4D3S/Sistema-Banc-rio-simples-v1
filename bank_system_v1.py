d140075709d4fbc8d7df77dcea89f601ce3e78c6
Izaac Emanoel <147447096+yH4D3S@users.noreply.github.com>
2025-09-22 10:37:52 -0300

    Implement daily withdrawal limit and update transaction logic
    Added limit tracking for daily withdrawals and updated deposit and withdrawal logic to include limit checks.

--- bank_system_v1.py
+++ bank_system_v1.py
@@ -10,6 +10,7 @@ Bem vindo(a) ao sistema báncario de Izaac!!
 
 saldo =  0
 limite = 500
+limite_usado = 0
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
@@ -22,28 +23,36 @@ while True:
         valor_depositado = float(input('Digite um valor a ser dépositado: '))
         if (valor_depositado > 0):
             saldo += valor_depositado
-            extrato = f"Você tem um total de R$ {saldo:.2f}".replace(".", ",")
+            extrato += f"Depósito: R$ {valor_depositado:.2f}\n"
             print(f"Você depósitou um total de R$ {valor_depositado:.2f}, seu saldo atual é R$ {saldo:.2f}".replace(".", ","))
         else:
                 print('Operação inválida, por favor digite um número válido para déposito')
     elif (opcao == 's'):
+
         saque = float(input('Insira a quantia que você, deseja sacar: '))
+
         if (numero_saques == LIMITE_SAQUES):
             print('Você atingiu o número de saques máximos diários.')
         else:
-            if ((saldo >= saque) and (saque > 0)):
+            
+            if ((saldo >= saque) and (saque > 0) and (limite_usado + saque) <= limite):
                 saldo -= saque
-                extrato = f"Você tem um total de R$ {saldo:.2f}".replace(".", ",")
+                limite_usado += saque
+                extrato += f"Saque: R$ {saque:.2f}\n"
                 print(f"Você sacou um total de R$ {saque:.2f}, seu saldo atual é R$ {saldo:.2f}".replace(".", ","))
                 numero_saques += 1
 
             elif (saque > saldo):
+
                 print('Saldo insuficiente!')
+
+            elif (limite_usado >= limite): 
+                print("Você excedeu antigiu o limite de quantia retirada diária.")
 
             else:
                 print('Operação inválida, por favor digite um número válido para saque')
     elif(opcao == 'e'):
-        print(f"Você tem um total de R$ {saldo:.2f}".replace(".", ","))
+        print(extrato + f"\nVocê tem um total de R$ {saldo:.2f}".replace(".", ","))
     elif (opcao == 'q'):
         break
     
